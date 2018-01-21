042812994b40a6763b7f7b8b378745f91bbbe3d1
Himanshu44ec06 <himanshu.44ec06@gmail.com>
2018-01-21 13:02:16 +0530

    Error Handling  in  app

--- config/constant.js
+++ config/constant.js
@@ -4,12 +4,23 @@ var  constant  = (function () {
     var constant ;
     function setConstant() {
 
+     var  env  = process.env.NODE_ENV || 'DEV';
+       if(env == 'DEV')
+        {
         return {
             PORT :  process.env.PORT || 8089,
             MongoConnection :  "mongodb://localhost:27017/products",
             BasePath : "/api",
             KEY : 'adcf%23%^2Df@123!@#'
-        }
+        }}else  {
+
+           return {
+               PORT :  process.env.PORT || 8089,
+               MongoConnection :  "mongodb://localhost:27017/products",
+               BasePath : "/api",
+               KEY : 'adcf%23%^2Df@123!@#'
+           }
+       }
     }
 
     return  {
